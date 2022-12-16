02d625a6e9966ffa631a8c2e690b34a6b58dc7ce
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-12-16 18:28:33 +0100

    Fixed music playback & optimized load times

--- index.js
+++ index.js
@@ -481,8 +481,8 @@ function spawnWindow(content) {
 
 function toggleColorScheme() {
     if (colorscheme === "dark") {
-        document.documentElement.style.setProperty('--bg-img', 'url("assets/background/light.png")');
-        document.documentElement.style.setProperty('--bg-img-blur', 'url("assets/background/lightblur.png")');
+        document.documentElement.style.setProperty('--bg-img', 'var(--asset-bg-img-light)');
+        document.documentElement.style.setProperty('--bg-img-blur', 'var(--asset-bg-img-blur-light)');
 
         document.documentElement.style.setProperty('--navbar-btn-hover-color', 'rgba(0 0 0 / 20%)');
         document.documentElement.style.setProperty('--navbar-bg-color', ' rgba(255 255 255 / 60%)');
@@ -498,8 +498,8 @@ function toggleColorScheme() {
         document.getElementById("toggleColorScheme").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z"></path></svg>`
 
     } else {
-        document.documentElement.style.setProperty('--bg-img', 'url("assets/background/dark.png")');
-        document.documentElement.style.setProperty('--bg-img-blur', 'url("assets/background/darkblur.png")');
+        document.documentElement.style.setProperty('--bg-img', 'var(--asset-bg-img-dark)');
+        document.documentElement.style.setProperty('--bg-img-blur', 'var(--asset-bg-img-blur-dark)');
 
         document.documentElement.style.setProperty('--navbar-btn-hover-color', 'rgba(255 255 255 / 20%)');
         document.documentElement.style.setProperty('--navbar-bg-color', ' rgba(0 0 0 / 60%)');
@@ -556,5 +556,5 @@ window.addEventListener('resize', function (event) {
 });
 
 
-//spawnWindow(windowBuilder('assets/icons/github.png', 'GitHub', windowGithub));
+spawnWindow(windowBuilder('assets/icons/github.png', 'GitHub', windowGithub));
 
