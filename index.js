64136790b70b35d93fc0bb7dff2c1e43fe503496
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-03-29 15:36:29 +0200

    Push

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ function taskBarAppHandleDragStart(event) {
     const move = (event) => {
         let btnSize = 50;
         let maxAmount = Number(getComputedStyle(taskBar).getPropertyValue('--app-amount'));
-        
+
         let newDraggedTaskBarIconIndex = Math.floor((event.clientX - taskBar.getBoundingClientRect().left) / btnSize);
         let draggedTaskBarIconOffset = (-((taskBar.getBoundingClientRect().left + (newDraggedTaskBarIconIndex * btnSize)) - event.clientX) / btnSize) - 0.5;
-        if(newDraggedTaskBarIconIndex < 0 || (newDraggedTaskBarIconIndex == 0 && draggedTaskBarIconOffset < 0)) draggedTaskBarIconOffset = 0;
-        if(newDraggedTaskBarIconIndex > maxAmount - 1 || (newDraggedTaskBarIconIndex == maxAmount - 1 && draggedTaskBarIconOffset > 0)) draggedTaskBarIconOffset = 0;
+        if (newDraggedTaskBarIconIndex < 0 || (newDraggedTaskBarIconIndex == 0 && draggedTaskBarIconOffset < 0)) draggedTaskBarIconOffset = 0;
+        if (newDraggedTaskBarIconIndex > maxAmount - 1 || (newDraggedTaskBarIconIndex == maxAmount - 1 && draggedTaskBarIconOffset > 0)) draggedTaskBarIconOffset = 0;
         el.style.setProperty('--offset', draggedTaskBarIconOffset);
 
-        if(newDraggedTaskBarIconIndex != draggedTaskBarIconIndex){
+        if (newDraggedTaskBarIconIndex != draggedTaskBarIconIndex) {
             el.style.setProperty('--index', Math.min(maxAmount - 1, Math.max(0, newDraggedTaskBarIconIndex)));
             //Rebuild all indexes
             let icons = [...taskBar.children];
             //Remove the dragged item from the list
-            for(let i = 0; i < icons.length; i++){
-                if(icons[i] === el){
+            for (let i = 0; i < icons.length; i++) {
+                if (icons[i] === el) {
                     icons.splice(i, 1);
                 }
             }
@@ -37,9 +37,9 @@ function taskBarAppHandleDragStart(event) {
             });
             let i = -1;
             icons.splice(Math.min(maxAmount - 1, Math.max(0, newDraggedTaskBarIconIndex)), 0, el)
-            for(let icon of icons){
+            for (let icon of icons) {
                 i++;
-                if(icon === el) continue;
+                if (icon === el) continue;
                 icon.style.setProperty('--index', i);
             }
 
@@ -65,6 +65,327 @@ for (let taskBarIcon of taskBar.children) {
     });
 }
 
-class Window{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadwindowtext(btn, id) {
+    let elements = btn.parentElement.parentElement.parentElement.getElementsByClassName("details");
+    Array.from(elements).forEach(function (item, index) {
+        if (item.classList.contains(id)) {
+            item.style.display = "initial";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
+function windowBuilder(img, title, content) {
+    return /*html*/`
+
+    <div class="window-container" style="--w: 1048px; --h: 700px; --window-border-radius: 20px;">
+        <div class="fwrapper window-inner-wrapper">
+            <div class="window-border"></div>
+            <div class="window-pane">
+                <div class="fwrapper">
+
+                    <div class="topbar">
+                        <div class="title">
+                        <img src="${img}">
+                            <p>${title}</p>
+                        </div>
+                        <div class="controls">
+                            <button class="min"><img src="assets/icons/minus.svg"></button>
+                            <button class="max"><img src="assets/icons/square-rounded.svg"></button>
+                            <button class="close"><img src="assets/icons/x-circle.svg"></button>
+                        </div>
+                    </div>
+
+                    <div class="content-pane">
+                        ${content}
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
     
+    `;
+}
+
+
+
+
+function last(array) {
+    return array[array.length - 1];
+}
+
+
+
+const windowGithub = /*html*/`
+<h1 align="center">Hi <span class="wave">👋</span>, I am FabulousFox</h1>
+<h3 align="center">I mostly code useless stuff</h3>
+<p align="center">(<a href="https://github.com/FabulousCodingFox" target="_blank">https://github.com/FabulousCodingFox</a> || <a href="https://fabulouscodingfox.github.io/" target="_blank">https://fabulouscodingfox.github.io/</a>)</p>
+<hr>
+`
+
+
+let windowY = 1000;
+
+let dragged;
+let resized;
+let resizedSide;
+
+let mouseX = 0;
+let mouseY = 0;
+
+var colorscheme = "dark"
+
+class Window {
+    constructor(content) {
+        var root = this;
+
+        let width = 0.6 * (window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth);
+        let height = 0.8 * (window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight);
+
+        this.windowWidth = width;
+        this.windowHeight = height;
+        this.windowPosX = window.innerWidth / 2 - width / 2;
+        this.windowPosY = window.innerHeight / 2 - height / 2;
+        this.windowContent = content;
+        this.isMaxximized = false
+        this.isMinimized = false
+
+        document.querySelector("body").insertAdjacentHTML('beforeend', this.windowContent);
+        this.wd = last(document.querySelector("body").getElementsByClassName("window-container"));
+        this.windowElement = this.wd.getElementsByClassName("window-inner-wrapper")[0];
+        this.topbar = this.windowElement.getElementsByClassName("topbar")[0];
+        this.closebtn = this.topbar.getElementsByClassName("controls")[0].getElementsByClassName("close")[0];
+        this.maxbtn = this.topbar.getElementsByClassName("controls")[0].getElementsByClassName("max")[0];
+        this.minbtn = this.topbar.getElementsByClassName("controls")[0].getElementsByClassName("min")[0];
+
+        this.wd.style.left = this.windowPosX + "px";
+        this.wd.style.top = this.windowPosY + "px";
+        this.wd.style.setProperty('--w', width + "px");
+        this.wd.style.setProperty('--h', height + "px");
+        this.wd.style.setProperty('--window-border-radius', "20px")
+        this.wd.style.setProperty('--sidebar', "280px")
+        this.windowElement.style.cursor = "nwse-resize"
+
+        this.layer = windowY
+        this.wd.style.zIndex = windowY;
+        windowY++;
+
+        this.mouseHoversOnContent = false;
+
+        //Resize Window
+        this.windowContent.onmouseenter = function () {
+            root.mouseHoversOnContent = true;
+        };
+        this.windowContent.onmouseleave = function () {
+            root.mouseHoversOnContent = false;
+        }
+        this.windowElement.addEventListener("mousemove", (event) => {
+            if (resized != undefined) return;
+            if (this.mouseHoversOnContent) return;
+
+            let r = this.windowElement.getBoundingClientRect();
+
+            let n = Math.abs(r.top - mouseY) <= 20
+            let s = Math.abs(r.bottom - mouseY) <= 20
+            let w = Math.abs(r.left - mouseX) <= 20
+            let e = Math.abs(r.right - mouseX) <= 20
+
+            if (n && e) { resizedSide = "ne"; this.windowElement.style.cursor = "nesw-resize" }
+            else if (n && w) { resizedSide = "nw"; this.windowElement.style.cursor = "nwse-resize" }
+            else if (n) { resizedSide = "n"; this.windowElement.style.cursor = "ns-resize" }
+            else if (s && e) { resizedSide = "se"; this.windowElement.style.cursor = "nwse-resize" }
+            else if (s && w) { resizedSide = "sw"; this.windowElement.style.cursor = "nesw-resize" }
+            else if (s) { resizedSide = "s"; this.windowElement.style.cursor = "ns-resize" }
+            else if (w) { resizedSide = "w"; this.windowElement.style.cursor = "ew-resize" }
+            else if (e) { resizedSide = "e"; this.windowElement.style.cursor = "ew-resize" }
+            else { resizedSide = "NONE"; }
+        });
+        this.windowElement.addEventListener("mousedown", (event) => {
+            if (this.mouseHoversOnContent) return;
+            resized = root;
+        });
+
+        //Maximize window
+        this.topbar.ondblclick = () => {
+            this.setMaxximized(!this.isMaxximized);
+        };
+
+        this.maxbtn.onclick = () => {
+            this.setMaxximized(!this.isMaxximized);
+        };
+
+        //Close window
+        this.closebtn.onclick = () => {
+            this.wd.style.opacity = '0%';
+            const temp = this.wd;
+            setTimeout(function () { temp.remove(); }, 500);
+        };
+        this.minbtn.onclick = () => {
+            this.wd.style.opacity = '0%';
+            const temp = this.wd;
+            setTimeout(function () { temp.remove(); }, 500);
+        };
+
+        //Focus Window
+        this.wd.addEventListener("mousedown", (event) => {
+            if (this.wd.style.zIndex == windowY - 1) return;
+            this.wd.style.zIndex = windowY;
+
+            /*this.wd.classList.remove('windowAppear');
+            void this.wd.offsetWidth;
+            this.wd.classList.add('windowAppear'); */
+
+            windowY++;
+        });
+
+        //Move window
+        this.topbar.addEventListener("mousedown", (event) => {
+            if (resized != undefined) return;
+            dragged = this;
+            //wd.classList.remove('windowAppear');
+        });
+    }
+
+    setMaxximized(m) {
+        if (!m) {
+            this.wd.style.left = this.windowPosX + "px";
+            this.wd.style.top = this.windowPosY + "px";
+            this.wd.style.setProperty('--w', this.windowWidth + "px");
+            this.wd.style.setProperty('--h', this.windowHeight + "px");
+            this.wd.style.setProperty('--window-border-radius', "20px")
+        } else {
+            this.wd.style.left = "0px";
+            this.wd.style.top = "0px";
+            this.wd.style.setProperty('--w', window.innerWidth + "px");
+            this.wd.style.setProperty('--h', window.innerHeight + "px");
+            this.wd.style.setProperty('--window-border-radius', "0px")
+        }
+        this.isMaxximized = m;
+    }
+
+    setPos(x, y) {
+        this.windowPosX = x;
+        this.windowPosY = y;
+        this.wd.style.left = this.windowPosX + "px";
+        this.wd.style.top = this.windowPosY + "px";
+    }
+
+    setDimensions(w, h) {
+        this.windowWidth = w;
+        this.windowHeight = h;
+        this.wd.style.setProperty('--w', w + "px");
+        this.wd.style.setProperty('--h', h + "px");
+    }
+}
+
+document.addEventListener("mouseup", (event) => {
+    dragged = undefined;
+    resized = undefined;
+});
+
+document.addEventListener("mousemove", (event) => {
+    let offX = event.clientX - mouseX;
+    let offY = event.clientY - mouseY;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+
+    if (dragged != undefined) {
+        if (dragged.isMaxximized) {
+            dragged.setMaxximized(false);
+            dragged.setPos(event.clientX - dragged.windowWidth / 2, event.clientY - 20);
+        }
+        let currX = dragged.windowPosX;
+        let currY = dragged.windowPosY;
+        dragged.setPos(currX + offX, currY + offY)
+    }
+
+    if (resized != undefined) {
+        let currX = resized.windowPosX;
+        let currY = resized.windowPosY;
+        let currW = resized.windowWidth;
+        let currH = resized.windowHeight;
+
+        if (resizedSide === "ne") {
+            resized.setPos(currX, currY + offY)
+            resized.setDimensions(currW + offX, currH - offY)
+        }
+        if (resizedSide === "nw") {
+            resized.setPos(currX + offX, currY + offY)
+            resized.setDimensions(currW - offX, currH - offY)
+        }
+        if (resizedSide === "n") {
+            resized.setPos(currX, currY + offY)
+            resized.setDimensions(currW, currH - offY)
+        }
+        if (resizedSide === "se") {
+            resized.setDimensions(currW + offX, currH + offY)
+        }
+        if (resizedSide === "sw") {
+            resized.setPos(currX + offX, currY)
+            resized.setDimensions(currW - offX, currH + offY)
+        }
+        if (resizedSide === "s") {
+            resized.setDimensions(currW, currH + offY)
+        }
+        if (resizedSide === "w") {
+            resized.setPos(currX + offX, currY)
+            resized.setDimensions(currW - offX, currH)
+        }
+        if (resizedSide === "e") {
+            resized.setDimensions(currW + offX, currH)
+        }
+    }
+});
+
+function spawnWindow(content) {
+    new Window(content);
 }
