f7fbce76afca6f39be22294c997dddda9f2fe854
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-04-02 12:22:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -121,48 +121,19 @@ function desktopAppHandleDragStart(event) {
         let newDraggedDesktopIconX = Math.floor((event.clientX - desktop_apps.getBoundingClientRect().left) / el.clientWidth);
         let newDraggedDesktopIconY = Math.floor((event.clientY - desktop_apps.getBoundingClientRect().top) / el.clientHeight);
 
-        let newDraggedDesktopIconOffsetX = (-((desktop_apps.getBoundingClientRect().left + (newDraggedDesktopIconX * el.clientWidth)) - event.clientX) / el.clientWidth) - 0.5;
-        let newDraggedDesktopIconOffsetY = (-((desktop_apps.getBoundingClientRect().top + (newDraggedDesktopIconY * el.clientHeight)) - event.clientY) / el.clientHeight) - 0.5;
-        el.style.transform = `translate(${newDraggedDesktopIconOffsetX}%, ${newDraggedDesktopIconOffsetY}%)`;
+        let newDraggedDesktopIconOffsetX = ((-((desktop_apps.getBoundingClientRect().left + (newDraggedDesktopIconX * el.clientWidth)) - event.clientX) / el.clientWidth) - 0.5) * el.clientWidth;
+        let newDraggedDesktopIconOffsetY = ((-((desktop_apps.getBoundingClientRect().top + (newDraggedDesktopIconY * el.clientHeight)) - event.clientY) / el.clientHeight) - 0.5) * el.clientHeight;
+        el.style.transform = `translate(${newDraggedDesktopIconOffsetX}px, ${newDraggedDesktopIconOffsetY}px)`;
 
         if (newDraggedDesktopIconX != draggedDesktopIconX || newDraggedDesktopIconY != draggedDesktopIconY) {
             el.style.setProperty('--x', newDraggedDesktopIconX);
             el.style.setProperty('--y', newDraggedDesktopIconY);
+
+            draggedDesktopIconX = newDraggedDesktopIconX;
+            draggedDesktopIconY = newDraggedDesktopIconY;
         }
 
         return;
-
-        let newDraggedTaskBarIconIndex = Math.floor((event.clientX - taskBar.getBoundingClientRect().left) / btnSize);
-        let draggedTaskBarIconOffset = (-((taskBar.getBoundingClientRect().left + (newDraggedTaskBarIconIndex * btnSize)) - event.clientX) / btnSize) - 0.5;
-        if (newDraggedTaskBarIconIndex < 0 || (newDraggedTaskBarIconIndex == 0 && draggedTaskBarIconOffset < 0)) draggedTaskBarIconOffset = 0;
-        if (newDraggedTaskBarIconIndex > maxAmount - 1 || (newDraggedTaskBarIconIndex == maxAmount - 1 && draggedTaskBarIconOffset > 0)) draggedTaskBarIconOffset = 0;
-        el.style.setProperty('--offset', draggedTaskBarIconOffset);
-
-        if (newDraggedTaskBarIconIndex != draggedTaskBarIconIndex) {
-            el.style.setProperty('--index', Math.min(maxAmount - 1, Math.max(0, newDraggedTaskBarIconIndex)));
-            //Rebuild all indexes
-            let icons = [...taskBar.children];
-            //Remove the dragged item from the list
-            for (let i = 0; i < icons.length; i++) {
-                if (icons[i] === el) {
-                    icons.splice(i, 1);
-                }
-            }
-            icons.sort((a, b) => {
-                let aIndex = Number(getComputedStyle(a).getPropertyValue('--index'));
-                let bIndex = Number(getComputedStyle(b).getPropertyValue('--index'));
-                return aIndex - bIndex;
-            });
-            let i = -1;
-            icons.splice(Math.min(maxAmount - 1, Math.max(0, newDraggedTaskBarIconIndex)), 0, el)
-            for (let icon of icons) {
-                i++;
-                if (icon === el) continue;
-                icon.style.setProperty('--index', i);
-            }
-
-            draggedTaskBarIconIndex = newDraggedTaskBarIconIndex;
-        }
     };
 
     const up = () => {
